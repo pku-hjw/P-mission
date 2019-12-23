88a4968b6322ba25443385a01abbfad3305bf053
sspku-yqLiu <56879942+sspku-yqLiu@users.noreply.github.com>
2019-12-23 11:11:26 +0800

    更新前端>>任务与发布

--- p-mission/config/api.js
+++ p-mission/config/api.js
@@ -1,6 +1,7 @@
 // 以下是业务服务器API地址
 // 本机开发时使用
-var WxApiRoot = 'http://localhost:8081/wx/';
+// var WxApiRoot = 'http://localhost:8081/wx/';
+var WxApiRoot = 'http://localhost:8080';
 // 局域网测试使用
 //var WxApiRoot = 'http://182.254.200.15:8081/wx/';
 // 云平台上线时使用
@@ -23,6 +24,7 @@ module.exports = {
   GoodsCategory: WxApiRoot + 'goods/category', //获得分类数据
   GoodsDetail: WxApiRoot + 'goods/detail', //获得商品的详情
   GoodsRelated: WxApiRoot + 'goods/related', //商品详情页的关联商品（大家都在看）
+  GoodsPost: WxApiRoot +'/goods/addgood',//发布商品
 
   BrandList: WxApiRoot + 'brand/list', //品牌列表
   BrandDetail: WxApiRoot + 'brand/detail', //品牌详情
@@ -36,7 +38,11 @@ module.exports = {
   CartGoodsCount: WxApiRoot + 'cart/goodscount', // 获取购物车商品件数
   CartCheckout: WxApiRoot + 'cart/checkout', // 下单前信息确认
 
-  CommentPost: WxApiRoot + 'comment/post', //发表评论
+  CommentPost: WxApiRoot + '/comment/addComment', //发表评论
+  CommentGet: WxApiRoot +'/comment/getcomment', //获取评论
+
+  GetAllMission: WxApiRoot +'/mission/getAllMission',//获取全部任务
+  GetMission: WxApiRoot +'/mission/getMission',//获取单个任务
 
   TopicList: WxApiRoot + 'topic/list', //专题列表
   TopicDetail: WxApiRoot + 'topic/detail', //专题详情
--- p-mission/pages/depoly/depoly.js
+++ p-mission/pages/depoly/depoly.js
@@ -1,16 +1,24 @@
+// goLogin() {
+//   wx.navigateTo({
+//     url: "/pages/auth/login/login"
+//   });
+// },
+// hasLogin: false,
+
 const qiniuUploader = require("../../utils/qiniuUploader");
 var util = require('../../utils/util.js');
 var api = require('../../config/api.js');
 var user = require('../../utils/user.js');
 
-var app = getApp();
+// const qiniuUploader = require("../../utils/qiniuUploader");
+//index.js
 
 // 初始化七牛相关参数
 function initQiniu() {
   var app = getApp()
   var options = {
     region: 'ECN', // 华北区
-    uptokenURL: app.globalData.domain.dev +'/goods/image/uptoken',
+    uptokenURL: app.globalData.domain.dev + '/goods/image/uptoken',
     // uptoken: 'qVwP0HV-VC7jGGmREBUZwgKrdJNs9ib5_rA-6OSs:nEn4E36d7z8dmk9qjpdNyoIk5qg=:eyJzY29wZSI6ImF5eS1maWxlIiwiZGVhZGxpbmUiOjE1MTQwMjUyMzl9',
     domain: app.globalData.domain.qiniuImg,
     shouldUseQiniuFileName: false
@@ -22,54 +30,47 @@ function initQiniu() {
 var app = getApp()
 Page({
   data: {
+    /* 0 表示商品 1 表示任务*/
     hasLogin: false,
+    switchstatus: 0,
     imageList: [],
-    categories:["衣服","鞋子","书本","电器","其他"],
-    cIndex:0,
+    categories: ["衣服", "鞋子", "书本", "电器", "其他"],
+    mission_categories: ["着急要", "慢慢收"],
+    cIndex: 0,
+    mission_index: 0,
     donationList: ["否", "是"],
-    dIndex: 0,    
-    location:'',
+    dIndex: 0,
+    location: '',
     title: '',
-    desc: '',
-    price:{
+    discribe: '',
+    mission_price: '',
+    price: {
       now: "",
       old: "",
       freight: ""
     },
-    tagStr:''
+    tagStr: '',
+    sort_button_back_color: ["", ""],
+    pages: [],
+    addType: 1,
+    hasMore: true,
   },
 
   //事件处理函数
-  onLoad: function (options) {
+  onLoad: function () {
+    console.log('onLoad')
     var that = this;
 
     //定位
     that.getCityNameOFLocation()
   },
-
-  onReady: function () {
-    // 页面渲染完成
-  },
-  onShow: function () {
-    this.setData({
-      hasLogin: app.globalData.hasLogin
-    });
-
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  },
   goLogin() {
-    wx.navigateTo({
-      url: "/pages/auth/login/login"
-    });
-  },
-
-  
-  didPressChooesImage: function() {
+  wx.navigateTo({
+    url: "/pages/auth/login/login"
+  });
+},
+hasLogin: false,
+  didPressChooesImage: function () {
     var _this = this;
     didPressChooesImage(_this, function (result) {
       _this.data.imageList.push(result.imageURL);
@@ -79,17 +80,22 @@ Page({
     }, function (error) {
     });
   },
-  bindTitleInputChange: function(e) {
+  bindTitleInputChange: function (e) {
     this.setData({
       'title': e.detail.value
     })
   },
-  bindDescInputChange: function(e) {
+  bindDiscribeInputChange: function (e) {
     this.setData({
-      'desc': e.detail.value
+      'discribe': e.detail.value
     })
   },
-  clickEditPrice: function(e) {
+  bindPriceInputChange: function (e) {
+    this.setData({
+      'mission_price': e.detail.value
+    })
+  },
+  clickEditPrice: function (e) {
     var _that = this;
     wx.navigateTo({
       url: 'edit-price/edit-price?' + 'now=' + _that.data.price.now + '&old=' + _that.data.price.old + '&freight=' + _that.data.price.freight,
@@ -101,35 +107,71 @@ Page({
       url: 'edittag/edittag?tagStr=' + _that.data.tagStr,
     })
   },
-  bindCategoryChange: function(e) {
+  bindCategoryChange: function (e) {
     this.setData({
       cIndex: e.detail.value
     })
   },
-  bindPickerChange: function(e) {
+  bindPickerChange: function (e) {
     this.setData({
       dIndex: e.detail.value
     })
   },
-  publishGoods: function(e) {
+  /**
+   * 测试函数
+   */
+  test: function () {
+    wx.navigateTo({
+      url: '../add/addmission/addmission'
+    })
+    console.log("test")
+  },
+  /*两个发布函数*/
+  publish_mission: function () {
+    this.setData({
+      sort_button_back_color: ["#C0C4CC", "#fff"],
+      switchstatus: 0
+    }),
+      console.log("publishmission"),
+      wx.navigateTo({
+        url: '../index/index',
+        success: function (res) {
+          console.log("success")
+        },
+        fail: function (res) { },
+        complete: function (res) { },
+      })
+  },
+  publish_goods: function () {
+    this.setData({
+      switchstatus: 1,
+      sort_button_back_color: ["#fff", "#C0C4CC"],
+      text_msg: "publishgoods"
+    })
+    console.log("publishgoods")
+  },
+  publishGoods: function (e) {
     var _that = this;
     var app = getApp();
+    console.log(_that.data.discribe)
+    console.log(_that.data.title)
     wx.request({
-      url: app.globalData.domain.dev + 'goods/add/',
+      url: 'http://localhost:8080/goods/addgood',
+
       method: 'POST',
       header: {
         "Content-Type": "application/x-www-form-urlencoded"
       },
-      data:{
-        'user_id': app.globalData.userInfo.userId,
+      data: {
+        'publisher_id': app.globalData.userInfo.userId,
+        'publisher': app.globalData.userInfo.nickname,
         'title': _that.data.title,
-        'descible': _that.data.desc,
+        'desc': _that.data.discribe,
         'price': _that.data.price.now,
         'old_price': _that.data.price.old,
-        'freight': _that.data.price.freight,
-        'image_list': _that.data.imageList,
-        'publish_address': _that.data.location,
-        'category': _that.data.cIndex,
+        'cover': _that.data.imageList,
+        'address': _that.data.location,
+        'tags': _that.data.tagStr,
         'is_donation': _that.data.dIndex
       },
       success: function (res) {
@@ -137,7 +179,44 @@ Page({
         wx.showToast({
           title: '发布成功',
           icon: 'success',
-          duration: 1500
+          duration: 10000
+        })
+        wx.reLaunch({
+          url: '/pages/index/index',
+        })
+      }
+    })
+  },
+  publishMission: function (e) {
+    var _that = this;
+    var app = getApp();
+    console.log(_that.data.discribe)
+    console.log(_that.data.title)
+    wx.request({
+      url: 'http://localhost:8080/mission/addmissison',
+      method: 'POST',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: {
+        'publisherid': app.globalData.userInfo.userId,
+        'publisher': app.globalData.userInfo.nickname,
+        'missionname': _that.data.title,
+        'discribe': _that.data.discribe,
+        'price': _that.data.mission_price,
+        'old_price': _that.data.price.old,
+        'freight': _that.data.price.freight,
+        'image_list': _that.data.imageList,
+        'publisher_location': _that.data.location,
+        'missionclass': _that.data.cIndex,
+        'is_donation': _that.data.dIndex
+      },
+      success: function (res) {
+        //console.log(JSON.parse(res))
+        wx.showToast({
+          title: '发布成功',
+          icon: 'success',
+          duration: 10000
         })
         wx.reLaunch({
           url: '/pages/index/index',
@@ -146,7 +225,6 @@ Page({
     })
   },
   getCityNameOFLocation: function () {
-
     var that = this;
     wx.getLocation({
       type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
@@ -205,7 +283,7 @@ function formatLocationDesc() {
     var province = app.globalData.userInfo.province;
     var city = app.globalData.userInfo.city;
   }
-  var location = province.substring(0, province.length - 1) + " "+ city.substring(0, city.length - 1);
+  var location = province.substring(0, province.length - 1) + " " + city.substring(0, city.length - 1);
   return location;
 }
 
@@ -213,17 +291,17 @@ function didPressChooesImage(that, resolve, reject) {
   initQiniu();
   // 微信 API 选文件
   wx.chooseImage({
-      count: 1,
-      success: function (res) {
-        var filePath = res.tempFilePaths[0];
-        // 交给七牛上传
-        qiniuUploader.upload(filePath, (res) => {
-          resolve(res);
-        }, (error) => {
-          reject(error);
-          // console.error('error: ' + JSON.stringify(error));
-        }
-        );
+    count: 1,
+    success: function (res) {
+      var filePath = res.tempFilePaths[0];
+      // 交给七牛上传
+      qiniuUploader.upload(filePath, (res) => {
+        resolve(res);
+      }, (error) => {
+        reject(error);
+        // console.error('error: ' + JSON.stringify(error));
       }
-    })
+      );
+    }
+  })
 }
